80c494458c43ab2ea3884b4914c7611b05d3b8fc
Sorbai <superbai@gmail.com>
2024-01-27 17:34:30 +0800

    [test] add proto test

---
+++ test/test_proto.cpp
@@ -0,0 +1,47 @@
+#include <iomanip>
+#include <iostream>
+
+#include "superbai.pb.h"
+
+void PrintHex(const std::string& str) {
+  for (const unsigned char c : str) {
+    std::cout << std::hex << std::setw(2) << std::setfill('0')
+              << static_cast<int>(c);
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  std::string pb_str;
+  superbai::sample::RosProtoTest proto_test;
+  // 0801
+  // 0000 1000 0000 0001
+  // tag-value
+  // tag 的二进制：000 1 000
+  //  wire_type的二进制： 000  0
+  // field_num的二进制：0001     1
+  // int32 num = 1;
+  // wire_type : 0
+  // field_num : 1
+
+  // 08c801
+  //  0000 1000    c801
+
+  // a00601
+  // 1010 0000 0000 0110 0000 0001
+  // 010 0000 000 0110
+  // 000 01100100  000
+
+  // proto_test.set_num(1);
+
+  // 12087375706572626169
+  // 0001 0010 0000 1000 01110011 0111 0101 01110000011001010111001
+  // tag: 001 0 010
+  // len: 000 1000 8
+  // value:01110011 ---115   s
+  // 0111 0101 ---117 u
+
+  proto_test.set_name("superbai");
+  proto_test.SerializeToString(&pb_str);
+  PrintHex(pb_str);
+}
